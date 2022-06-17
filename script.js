f330acd506daa1d7b5412f238ba096331fdc5de6
Diana Palafox <dianapalafox95@gmail.com>
2022-06-16 18:28:09 -0700

    Add a function that loops through the array and displays the books in a div

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 let myLibrary = []; 
 let buttonAddBook = document.querySelector('.add-book');
 let buttonSubmit = document.querySelector('.submit');
+let booksContainer = document.querySelector('.booksContainer'); 
 
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read 
-    this.info = function() {
-        console.log(title, author, pages, read)
-    }
 }
 
 function addBookToLibrary() {
@@ -20,14 +18,15 @@ function addBookToLibrary() {
         let read = document.getElementById('read').value; 
         let newBook = new Book(title, author, pages, read); 
         myLibrary.push(newBook); 
-        console.log(myLibrary); 
+        document.getElementById('form').style.display = 'none';
+        displayBooks();
     })
 }
 
 addBookToLibrary(); 
 
 function showForm() {
-   buttonAddBook.addEventListener('click', e => {
+   buttonAddBook.addEventListener('click', () => {
         document.getElementById('form').style.display = 'block'; 
    })
 }
@@ -35,12 +34,67 @@ showForm();
 
 
 function displayBooks() {
-    myLibrary.forEach(book => {
-        for (let key in book) {
-            console.log(`${key}: ${book[key]}`);
-        }
-    }); 
+    for(let i=0; i<myLibrary.length; i++) {
+        //Create the main div and the div that will store the information
+    
+        const infoContainer = document.createElement('div'); 
+        infoContainer.classList.add('info-container');
+
+        //Create the divs that will contain the author name, title and pages of the book
+        const bookTitle = document.createElement('div');
+        bookTitle.classList.add('book-info');
+
+        const bookAuthor = document.createElement('div');
+        bookAuthor.classList.add('book-info');
+
+        const bookPages = document.createElement('div'); 
+        bookPages.classList.add('book-info'); 
+        
+        //Create the spans for the headings
+        const titleHeading = document.createElement('div');
+        titleHeading.classList.add('heading-style');
+
+        const authorHeading = document.createElement('div');
+        authorHeading.classList.add('heading-author');
+
+        const pagesHeading = document.createElement('div');
+        pagesHeading.classList.add('heading-pages');
+
+        //Add the headings 
+        titleHeading.textContent = 'Title:';
+        authorHeading.textContent = 'Author:';
+        pagesHeading.textContent = 'Number of pages:'; 
+
+        const titleInput = document.createElement('div'); 
+        titleInput.classList.add('input-style');
+
+        const authorInput = document.createElement('div'); 
+        authorInput.classList.add('input-style');
+
+        const pagesInput = document.createElement('div');
+        pagesInput.classList.add('input-style'); 
+
+        titleInput.textContent = document.getElementById('title').value;
+        authorInput.textContent = document.getElementById('author').value; 
+        pagesInput.textContent = document.getElementById('pages').value;
+
+        infoContainer.appendChild(bookTitle);
+        infoContainer.appendChild(bookAuthor);
+        infoContainer.appendChild(bookPages); 
+
+        bookTitle.appendChild(titleHeading);
+        bookAuthor.appendChild(authorHeading);
+        bookPages.appendChild(pagesHeading);
+
+        bookTitle.appendChild(titleInput);
+        bookAuthor.appendChild(authorInput);
+        bookPages.appendChild(pagesInput);
+
+        booksContainer.appendChild(infoContainer); 
+
+    }   
+    
 }
 
-displayBooks();
+
 
